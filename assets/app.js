5f7af448fe45ba52894e705cf77852f98e81c7a2
re-z <re-z@ukr.net>
2017-11-01 19:22:52 +0200

    app in progress

--- assets/app.js
+++ assets/app.js
@@ -5,8 +5,8 @@ new Vue({
         monsterHealth: 100,
         visibleBtns: false,
         visibleStart: true,
-        damageLog: [1],
-        damageLog2: [2]
+        damageLog: [1,5],
+        damageLog2: [2,6]
 },
     methods: {
         showBtns: function(ev){
@@ -18,16 +18,55 @@ new Vue({
             this.monsterHealth -= randNum;
             var randNum2 = Math.floor(Math.random() * 10) + 1;
             this.yourHealth -= randNum2;
+	        if (this.yourHealth < 1) {
+		        alert("Game over. You losed")
+		        this.visibleBtns = !this.visibleBtns;
+		        this.visibleStart = !this.visibleStart;
+		        this.yourHealth = 100;
+		        this.monsterHealth = 100;
+	        }
+	        else if (this.monsterHealth < 1) {
+		        alert("Congratulations, you won!!!")
+		        this.visibleBtns = !this.visibleBtns;
+		        this.visibleStart = !this.visibleStart;
+		        this.yourHealth = 100;
+		        this.monsterHealth = 100;
+	        }
         },
         specialAtack: function () {
             this.monsterHealth -= 10;
             var randNum2 = Math.floor(Math.random() * 10) + 1;
             this.yourHealth -= randNum2;
+
+            if (this.yourHealth < 1) {
+                alert("Game over. You losed")
+	            this.visibleBtns = !this.visibleBtns;
+	            this.visibleStart = !this.visibleStart;
+	            this.yourHealth = 100;
+	            this.monsterHealth = 100;
+            }
+            else if (this.monsterHealth < 1) {
+                alert("Congratulations, you won!!!")
+	            this.visibleBtns = !this.visibleBtns;
+	            this.visibleStart = !this.visibleStart;
+	            this.yourHealth = 100;
+	            this.monsterHealth = 100;
+            }
+
         },
         heal: function() {
             var randNum2 = Math.floor(Math.random() * 10) + 1;
             this.yourHealth -= randNum2;
-            this.yourHealth += 10;
+            if(this.yourHealth > 90){
+	            this.yourHealth = 100;
+            }
+            if(this.yourHealth === 100 ) {
+                alert( "Your health is full" )
+            }
+            else {
+	            this.yourHealth += 10;
+            }
+
         },
         giveUp: function() {
             this.visibleBtns = !this.visibleBtns;
@@ -35,6 +74,11 @@ new Vue({
             this.yourHealth = 100;
             this.monsterHealth = 100;
             alert('What a shame! You gived up!')
+        },
+	    monsterHealth2: function () {
+            return {
+                width: this.monsterHealth + '%'
+            }
         }
 
 
