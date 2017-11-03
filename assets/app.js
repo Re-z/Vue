518a291549733eb515a0a64cc1c698ffb8c043cf
re-z <re-z@ukr.net>
2017-11-03 19:01:41 +0200

    fixed hit bar

--- assets/app.js
+++ assets/app.js
@@ -1,77 +1,73 @@
 new Vue({
     el: '.vue-game',
     data: {
-        yourHealth: 100,
+        playerHealth: 100,
         monsterHealth: 100,
-        visibleBtns: false,
         visibleStart: true,
         damageLog: [1,5],
         damageLog2: [2,6]
 },
     methods: {
         showBtns: function(ev){
-            this.visibleBtns = !this.visibleBtns;
             this.visibleStart = !this.visibleStart;
+	        this.playerHealth = 100;
+	        this.monsterHealth = 100;
         },
         atack: function() {
             var randNum = Math.floor(Math.random() * 10) + 1;
             this.monsterHealth -= randNum;
             var randNum2 = Math.floor(Math.random() * 10) + 1;
-            this.yourHealth -= randNum2;
-	        if (this.yourHealth < 1) {
+            this.playerHealth -= randNum2;
+	        if (this.playerHealth < 1) {
 		        alert("Game over. You losed")
 		        this.visibleBtns = !this.visibleBtns;
 		        this.visibleStart = !this.visibleStart;
-		        this.yourHealth = 100;
-		        this.monsterHealth = 100;
+
 	        }
 	        else if (this.monsterHealth < 1) {
 		        alert("Congratulations, you won!!!")
 		        this.visibleBtns = !this.visibleBtns;
 		        this.visibleStart = !this.visibleStart;
-		        this.yourHealth = 100;
-		        this.monsterHealth = 100;
+
 	        }
         },
         specialAtack: function () {
             this.monsterHealth -= 10;
             var randNum2 = Math.floor(Math.random() * 10) + 1;
-            this.yourHealth -= randNum2;
+            this.playerHealth -= randNum2;
 
-            if (this.yourHealth < 1) {
+            if (this.playerHealth < 1) {
                 alert("Game over. You losed")
 	            this.visibleBtns = !this.visibleBtns;
 	            this.visibleStart = !this.visibleStart;
-	            this.yourHealth = 100;
-	            this.monsterHealth = 100;
+
             }
             else if (this.monsterHealth < 1) {
                 alert("Congratulations, you won!!!")
 	            this.visibleBtns = !this.visibleBtns;
 	            this.visibleStart = !this.visibleStart;
-	            this.yourHealth = 100;
-	            this.monsterHealth = 100;
+
             }
 
         },
         heal: function() {
             var randNum2 = Math.floor(Math.random() * 10) + 1;
-            this.yourHealth -= randNum2;
-            if(this.yourHealth > 90){
-	            this.yourHealth = 100;
+            this.playerHealth -= randNum2;
+            if(this.playerHealth > 90){
+	            this.playerHealth = 100;
             }
-            if(this.yourHealth === 100 ) {
+            if(this.playerHealth === 100 ) {
                 alert( "Your health is full" )
             }
             else {
-	            this.yourHealth += 10;
+	            this.playerHealth += 10;
             }
 
         },
         giveUp: function() {
             this.visibleBtns = !this.visibleBtns;
             this.visibleStart = !this.visibleStart;
-            this.yourHealth = 100;
+            this.playerHealth = 100;
             this.monsterHealth = 100;
             alert('What a shame! You gived up!')
         },
