195b254b5948574bacf48978cee0eed809717f27
Re-z <re-z@ukr.net>
2017-11-12 20:09:20 +0200

    added heal log

--- assets/app.js
+++ assets/app.js
@@ -5,27 +5,28 @@ new Vue({
         monsterHealth: 100,
         visibleStart: true,
         turns: []
-},
+    },
     methods: {
-        showBtns: function(ev){
+        showBtns: function (ev) {
             this.visibleStart = !this.visibleStart;
-	        this.playerHealth = 100;
-	        this.monsterHealth = 100;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
             this.turns = [];
         },
-        atack: function() {
-            var playerDmg = this.generateRandom();
-            this.monsterHealth -= playerDmg;
-            this.turns.unshift({
-                isPlayer: true,
-                text: 'Player hits Monster for' + ' ' + playerDmg
-            });
-
+        atack: function () {
             var monsterDmg = this.generateRandom();
             this.playerHealth -= monsterDmg;
             this.turns.unshift({
-                isPlayer: false,
+                //isPlayer: false,
                 text: 'Monster hits Player for' + ' ' + monsterDmg
+            });
+
+
+            var playerDmg = this.generateRandom();
+            this.monsterHealth -= playerDmg;
+            this.turns.unshift({
+                //isPlayer: true,
+                text: 'Player hits Monster for' + ' ' + playerDmg
             });
             if (this.playerHealth < 1) {
                 this.endGame('Game over. You losed')
@@ -35,19 +36,18 @@ new Vue({
             }
         },
         specialAtack: function () {
-            this.monsterHealth -= 10;
-            this.turns.unshift({
-                isPlayer: true,
-                text: 'Player hits Monster with special attack for 10'
-            });
-
-
             var monsterDmg = this.generateRandom()
             this.playerHealth -= monsterDmg;
             this.turns.unshift({
-                isPlayer: false,
+                //isPlayer: false,
                 text: 'Monster hits Player for' + ' ' + monsterDmg
-            })
+            });
+
+            this.monsterHealth -= 10;
+            this.turns.unshift({
+                //isPlayer: true,
+                text: 'Player hits Monster with special attack for 10'
+            });
 
             if (this.playerHealth < 1) {
                 this.endGame('Game over. You losed')
@@ -56,33 +56,44 @@ new Vue({
                 this.endGame('Congratulations, you won!!!')
             }
         },
-        heal: function() {
-            this.playerHealth -= this.generateRandom();
-            if(this.playerHealth <= 90){
-	            this.playerHealth += 10;
+        heal: function () {
+            var monsterDmg = this.generateRandom()
+            this.playerHealth -= monsterDmg;
+            this.turns.unshift({
+                //isPlayer: false,
+                text: 'Monster hits Player for' + ' ' + monsterDmg
+            })
 
+            if (this.playerHealth <= 90) {
+                this.playerHealth += 10;
+                this.turns.unshift({
+                    isPlayer: true,
+                    text: 'Players gains 10 points of life'
+                })
             }
+
             else {
-	            this.playerHealth = 100;
+                this.playerHealth = 100;
+                this.turns.unshift({
+                    isPlayer: true,
+                    text: 'Player heals to 100'
+
+                })
             }
 
         },
-        giveUp: function() {
+        giveUp: function () {
             this.endGame('What a shame! You gived up!')
             this.playerHealth = 100;
             this.monsterHealth = 100;
             this.turns = [];
-
         },
-	    generateRandom: function () {
-            return  Math.floor(Math.random() * 10) + 1;
+        generateRandom: function () {
+            return Math.floor(Math.random() * 10) + 1;
         },
-        endGame: function(text) {
+        endGame: function (text) {
             alert(text);
             this.visibleStart = !this.visibleStart;
         }
-
-
-
     }
 });
