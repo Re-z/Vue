e2f15fb8a604754e00a0dbc3f142977a773a1004
Re-z <re-z@ukr.net>
2017-11-22 19:36:41 +0200

    prettyfied

--- assets/script.js
+++ assets/script.js
@@ -17,73 +17,60 @@ new Vue({
             var monsterDmg = this.generateRandom();
             this.playerHealth -= monsterDmg;
             this.turns.unshift({
-                //isPlayer: false,
                 text: 'Monster hits Player for' + ' ' + monsterDmg
             });
-
-
             var playerDmg = this.generateRandom();
             this.monsterHealth -= playerDmg;
             this.turns.unshift({
-                //isPlayer: true,
                 text: 'Player hits Monster for' + ' ' + playerDmg
             });
             if (this.playerHealth < 1) {
-                this.endGame('Game over. You losed')
+                this.endGame('Game over. You`ve losed')
             }
             else if (this.monsterHealth < 1) {
-                this.endGame('Congratulations, you won!!!')
+                this.endGame('Congratulations, you have won!!!')
             }
         },
         specialAtack: function () {
-            var monsterDmg = this.generateRandom()
+            var monsterDmg = this.generateRandom();
             this.playerHealth -= monsterDmg;
             this.turns.unshift({
-                //isPlayer: false,
                 text: 'Monster hits Player for' + ' ' + monsterDmg
             });
 
             this.monsterHealth -= 10;
             this.turns.unshift({
-                //isPlayer: true,
                 text: 'Player hits Monster with special attack for 10'
             });
 
             if (this.playerHealth < 1) {
-                this.endGame('Game over. You losed')
+                this.endGame('Game over. You`ve losed')
             }
             else if (this.monsterHealth < 1) {
-                this.endGame('Congratulations, you won!!!')
+                this.endGame('Congratulations, you have won!!!')
             }
         },
         heal: function () {
             var monsterDmg = this.generateRandom()
             this.playerHealth -= monsterDmg;
             this.turns.unshift({
-                //isPlayer: false,
                 text: 'Monster hits Player for' + ' ' + monsterDmg
-            })
-
+            });
             if (this.playerHealth <= 90) {
                 this.playerHealth += 10;
                 this.turns.unshift({
-                    isPlayer: true,
                     text: 'Players gains 10 points of life'
                 })
             }
-
             else {
                 this.playerHealth = 100;
                 this.turns.unshift({
-                    isPlayer: true,
                     text: 'Player heals to 100'
-
                 })
             }
-
         },
         giveUp: function () {
-            this.endGame('What a shame! You gived up!')
+            this.endGame('What a shame! You`ve gived up!')
             this.playerHealth = 100;
             this.monsterHealth = 100;
             this.turns = [];
